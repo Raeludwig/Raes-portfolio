7a6a6f41132646a6d4676210ee0a58130bb12e9b
Raeludwig <racheleludwig@gmail.com>
2023-05-09 09:59:40 -0600

    fixed grid, added cards

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -6,10 +6,21 @@ export default function Portfolio(props) {
     margin: "1rem",
   };
 
+  const images = [
+    process.env.PUBLIC_URL + "/job-finder.png",
+    process.env.PUBLIC_URL + "/moon-dollars.png",
+  ];
+
+  const gridStyle = {
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fit, minmax(18rem, 1fr))",
+    gap: "1rem",
+  };
+
   return (
     <div>
       <h1>Portfolio</h1>
-      <div className="container" style={{ display: "grid" }}>
+      <div className="container" style={gridStyle}>
         <div className="card" style={cardStyle}>
           <a href="https://raeludwig.github.io/Forked_Job_Finder/">
             <img
@@ -29,6 +40,78 @@ export default function Portfolio(props) {
             />
           </a>
         </div>
+
+        <div className="card" style={cardStyle}>
+          <a href="https://whispering-garden-84047.herokuapp.com/">
+            <img
+              className="card-img-top"
+              src={process.env.PUBLIC_URL + "/moon-dollars.png"}
+              alt="Card cap"
+            />
+          </a>
+        </div>
+
+        <div className="card" style={cardStyle}>
+          <a href="https://whispering-garden-84047.herokuapp.com/">
+            <img
+              className="card-img-top"
+              src={process.env.PUBLIC_URL + "/moon-dollars.png"}
+              alt="Card cap"
+            />
+          </a>
+        </div>
+
+        <div className="card" style={cardStyle}>
+          <a href="https://whispering-garden-84047.herokuapp.com/">
+            <img
+              className="card-img-top"
+              src={process.env.PUBLIC_URL + "/moon-dollars.png"}
+              alt="Card cap"
+            />
+          </a>
+        </div>
+
+        <div className="card" style={cardStyle}>
+          <a href="https://whispering-garden-84047.herokuapp.com/">
+            <img
+              className="card-img-top"
+              src={process.env.PUBLIC_URL + "/moon-dollars.png"}
+              alt="Card cap"
+            />
+          </a>
+        </div>
+
+        <div className="card" style={cardStyle}>
+          <a href="https://whispering-garden-84047.herokuapp.com/">
+            <img
+              className="card-img-top"
+              src={process.env.PUBLIC_URL + "/moon-dollars.png"}
+              alt="Card cap"
+            />
+          </a>
+        </div>
+
+        <div className="card" style={cardStyle}>
+          <a href="https://whispering-garden-84047.herokuapp.com/">
+            <img
+              className="card-img-top"
+              src={process.env.PUBLIC_URL + "/moon-dollars.png"}
+              alt="Card cap"
+            />
+          </a>
+        </div>
+
+        <div className="card" style={cardStyle}>
+          <a href="https://whispering-garden-84047.herokuapp.com/">
+            <img
+              className="card-img-top"
+              src={process.env.PUBLIC_URL + "/moon-dollars.png"}
+              alt="Card cap"
+            />
+          </a>
+        </div>
+
+
       </div>
     </div>
   );
