0e125eb97470ab2ca68b04e7f07b4de6a5743bfe
Rae Ludwig <racheleludwig@gmail.com>
2023-05-10 23:35:26 -0600

    fixed contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,26 +1,107 @@
 import React from 'react';
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 
-export default function Contact() {
+const ContactForm = ({ onSubmit }) => {
+  const [formStatus, setFormStatus] = React.useState('Send');
+  const [nameError, setNameError] = React.useState(false);
+  const [emailError, setEmailError] = React.useState(false);
+  const [messageError, setMessageError] = React.useState(false);
+  const nameRef = React.useRef(null);
+  const emailRef = React.useRef(null);
+  const messageRef = React.useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // validate inputs
+    let nameValid = nameRef.current.checkValidity();
+    let emailValid = emailRef.current.checkValidity();
+    let messageValid = messageRef.current.checkValidity();
+    setNameError(!nameValid);
+    setEmailError(!emailValid);
+    setMessageError(!messageValid);
+
+    if (nameValid && emailValid && messageValid) {
+      setFormStatus('Submitting...');
+      const formData = {
+        name: nameRef.current.value,
+        email: emailRef.current.value,
+        message: messageRef.current.value,
+      };
+      console.log(formData);
+      onSubmit(formData);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label className="form-label" htmlFor="name">
+          Name
+        </label>
+        <input
+          ref={nameRef}
+          className={`form-control ${nameError ? 'is-invalid' : ''}`}
+          type="text"
+          id="name"
+          required
+        />
+        {nameError && (
+          <div className="invalid-feedback">Please enter your name.</div>
+        )}
+      </div>
+      <div className="mb-3">
+        <label className="form-label" htmlFor="email">
+          Email
+        </label>
+        <input
+          ref={emailRef}
+          className={`form-control ${emailError ? 'is-invalid' : ''}`}
+          type="email"
+          id="email"
+          required
+        />
+        {emailError && (
+          <div className="invalid-feedback">Please enter a valid email.</div>
+        )}
+      </div>
+      <div className="mb-3">
+        <label className="form-label" htmlFor="message">
+          Message
+        </label>
+        <textarea
+          ref={messageRef}
+          className={`form-control ${messageError ? 'is-invalid' : ''}`}
+          id="message"
+          required
+        ></textarea>
+        {messageError && (
+          <div className="invalid-feedback">Please enter a message.</div>
+        )}
+      </div>
+      <button className="btn" type="submit">
+        {formStatus}
+      </button>
+    </form>
+  );
+};
+
+const Contact = () => {
+  const handleSubmit = (formData) => {
+    console.log('Submitted form data:', formData);
+    // Do something with the form data if needed
+  };
+
   return (
     <div>
-      <motion.h1 
-      initial={{y:-250}}
-      animate={{y: 0}}
-      >Contact Page</motion.h1>
-      <p class="container">
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+      <motion.h1 initial={{ y: -250 }} animate={{ y: 0 }}>
+        Contact Page
+      </motion.h1>
+      <div className="container mt-5">
+        <h2 className="mb-3">Send me a message!</h2>
+        <ContactForm onSubmit={handleSubmit} />
+      </div>
     </div>
   );
-}
+};
+
+export default Contact;
