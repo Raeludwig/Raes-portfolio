89d5d380c0173572fc00bb2da6776f4bfc512942
Rae Ludwig <racheleludwig@gmail.com>
2023-05-11 23:51:23 -0600

    fixed social icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,13 +9,13 @@ function FooterComponent() {
     <footer className="footer" class="Footer" position="fixed">
       <div className="icons social-icon">
         <a href="https://github.com/raeludwig" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faGithub} size="2x" style={{color: "#de5935",}}/>
+          <FontAwesomeIcon icon={faGithub} size="1x" style={{color: "#de5935",}}/>
         </a>
         <a href="https://twitter.com/Code_Rae"   target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faTwitter} size="2x"  style={{color: "#de5935",}}/>
+          <FontAwesomeIcon icon={faTwitter} size="1x"  style={{color: "#de5935",}}/>
         </a>
         <a href="https://www.linkedin.com/in/rae-ludwig-57164b51/"  target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faLinkedin} size="2x " style={{color: "#de5935",}} />
+          <FontAwesomeIcon icon={faLinkedin} size="1x " style={{color: "#de5935",}} />
         </a>
       </div>
     </footer>
