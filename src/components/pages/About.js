3ca008e8fed750321987abcc18ac5e05e3db7fd7
Raeludwig <racheleludwig@gmail.com>
2023-06-20 21:08:09 -0600

    new build

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -25,7 +25,7 @@ export default function About() {
       class="side"  style={imgStyle} src={process.env.PUBLIC_URL + "/rae.jpg"}
       />
       <p class="side side-content-center">
-      Hi! I am Rae. I am artist, turned librarian, turned web developer. Oddly enough I think that is a great combination of things. I have learned that aestetics and research are very important when it comes to coding. I am new to the coding world, in 2022 I had an issue with a program at work and accidentally ended up fixing the code to solve the problem (which is when I realized that coding is just MySpace! I hadn't actually realized that until that moment). I am currently enrolled in a full stack coding bootcamp through the UC San Diego and am excited to see where this journey takes me!  
+      Hi! I am Rae. I am artist, turned librarian, turned web developer. Oddly enough I think that is a great combination of things. I have learned that aestetics and research are very important when it comes to coding. I am new to the coding world, in 2022 I had an issue with a program at work and accidentally ended up fixing the code to solve the problem (which is when I realized that coding is just MySpace! I hadn't actually realized that until that moment). I just graduated June 2023 from a full stack coding bootcamp through the UC San Diego and am excited to see where this journey takes me!  
       </p>
     </div>
     </div>
